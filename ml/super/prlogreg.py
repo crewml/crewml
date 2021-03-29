815d3b1d7697abd2fdd5b78c41707e11ee738408
crewml <cybelink.webmaster@gmail.com>
2021-03-29 09:19:45 -0500

    xgboost impl added

--- ml/super/prlogreg.py
+++ ml/super/prlogreg.py
@@ -415,14 +415,15 @@ class PairingLogRegressor:
             "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
             "min_child_weight": [1, 3, 5, 7],
             "gamma": [0.0, 0.1, 0.2, 0.3, 0.4],
-            "colsample_bytree": [0.3, 0.4, 0.5, 0.7]
+            "colsample_bytree": [0.3, 0.4, 0.5, 0.7],
+            "objective": ["multi:softprob", "multi:softmax"]
 
         }
-        classifier = xgb.XGBClassifier()
+        classifier = xgb.XGBClassifier(use_label_encoder=False)
         random_search = RandomizedSearchCV(classifier,
                                            param_distributions=params,
                                            n_iter=5,
-                                           scoring='roc_auc',
+                                           scoring='accuracy',
                                            n_jobs=-1,
                                            cv=5,
                                            verbose=1)
